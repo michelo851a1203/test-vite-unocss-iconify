ffc9c7d17fef1f1a4f13d35d123403e74a652043
MichaelHo <michael@paradromix.com>
2022-09-29 17:35:43 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div
+    class="i-akar-icons-eye-open text-2xl"
+  ></div>
+<div
+    class="i-akar-icons-eye-slashed text-2xl"
+  ></div>
+</template>
+
+<style scoped></style>
